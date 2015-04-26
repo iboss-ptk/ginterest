8990fcf8ddd30f11ecc8238e945a7e7f61bd0808
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2015-04-26 18:20:40 +0700

    add display image to chat room

--- ginterest/urls.py
+++ ginterest/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from app import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'ginterest.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^time/', views.current_datetime),
+]
