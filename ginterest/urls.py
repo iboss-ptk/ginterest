4e7b0cf4a72486db426e3c99772bd50a37c3459d
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2015-04-27 11:42:16 +0700

    the api works

--- ginterest/urls.py
+++ ginterest/urls.py
@@ -1,11 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from app import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'ginterest.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^', include(router.urls)),
+    url(r'^', include('rest_framework.urls', namespace='rest_framework')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^time/', views.current_datetime),
