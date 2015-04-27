3c94df2e0530994f4d843ad422bab119b193a266
Fony Lewprasert <kamolphan@hotmail.com>
2015-04-27 12:10:34 +0700

    Merge branch 'master' of /home/fony/ginterest with conflicts.

--- app/models/customergroup.py
+++ app/models/customergroup.py
@@ -5,7 +5,7 @@ class CustomerGroup(models.Model):
     number_of_customer = models.IntegerField(default=1)
     queue_no = models.IntegerField(default=0)
     enter_time = models.DateTimeField(auto_now_add=True)
-    exit_time = models.DateTimeField(auto_now_add=False)
+    exit_time = models.DateTimeField()
 
     def __str__(self):
         return str(self.enter_time)+' ('+str(self.number_of_customer)+')'
