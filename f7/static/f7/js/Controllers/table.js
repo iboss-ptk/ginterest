c23ee377d94f8680a99f9bc255149baf8eac0a27
TeoGai <kmsunji@gmail.com>
2015-04-30 06:54:02 +0700

    UI -something wrong

--- f7/static/f7/js/Controllers/table.js
+++ f7/static/f7/js/Controllers/table.js
@@ -16,7 +16,7 @@ myApp.onPageInit('table-wait', function(page){
 				}	
 			}
 		})
-	},3000);
+	},500);
 });
 
 
@@ -76,102 +76,6 @@ myApp.onPageInit('place-order', function(page){
 				}
 			}
 		})
-		
-	$$('.order-submit-buton').on('click', function () {
-		for(var i in orderList){
-			var data = {
-				"orderlist_id": 	table_id,
-				"status": "q",
-				"comment": "...",
-				"quantity": 1,
-				"menu_id": orderList[i].id
-			}
-			$$.post('api/order/',data);
-		}
-		orderList = [];
-		$$('#menu-total').text(0);
-		$$('.place-order-col ul').children().remove();
-		myApp.alert('Order submitted');
-	});
-});
-
-
-myApp.onPageInit('table-history', function(page){
-	var total = 0;
-		$$.ajax({
-			url: 'api/order/', 
-			method: 'get',
-			success: function(result){
-				//console.log(result);
-				result = JSON.parse(result);
-				menu = result.results;
-				for(var i=0;i<menu.length;i++){
-					if(menu[i].orderlist_id == table_id){
-					  $$.get('api/menu/'+menu[i].menu_id+'/',null,function(data){
-						  data = JSON.parse(data);
-						  $$('.history-list ul').append(
-						  '<li>'+
-							'<div class="item-content">'+
-							  '<div class="item-media"><img src="static/f7/img/menu-'+data.id+'.jpg" width="44"/></div>'+
-							  '<div class="item-inner">'+
-								'<div class="item-title-row">'+
-								  '<div class="item-title color-black">'+data.name+'</div>'+
-								  '<div class="item-after">Amount: 1</div>'+
-								'</div>'+
-								'<div class="item-subtitle">Total: '+data.price+' Baht</div>'+
-							  '</div>'+
-							'</div>'+
-						  '</li>'
-						);
-						total = total + data.price;
-						$$('#history-total').text(total+' Baht');
-					  });
-					}
-				}
-			}
-		})
-		
-});
-
-myApp.onPageInit('table-check-out', function(page){
-	var total = 0;
-		$$.ajax({
-			url: 'api/order/', 
-			method: 'get',
-			success: function(result){
-				//console.log(result);
-				result = JSON.parse(result);
-				menu = result.results;
-				for(var i=0;i<menu.length;i++){
-					if(menu[i].orderlist_id == table_id){
-					  $$.get('api/menu/'+menu[i].menu_id+'/',null,function(data){
-						  data = JSON.parse(data);
-						  $$('.check-out-list ul').append(
-						  '<li>'+
-							  '<div class="item-content">'+
-								  '<div class="item-inner"> '+
-									'<div class="row max-width">'+		
-										'<div class="col-33 item-title">'+data.name+'</div>'+
-										'<div class="col-33 item-after align-right">1</div>'+
-										'<div class="col-33 item-after align-right">'+data.price+' Baht</div>'+
-									'</div>'+
-								  '</div>'+
-							  '</div>'+
-						  '</li>'
-						);
-						total = total + data.price;
-						$$('#check-out-total').text(total+'.00 Baht');
-						$$('#check-out-service').text(Math.round(total*0.1 * 100) / 100+' Baht');
-						$$('#check-out-vat').text(Math.round(total *0.07 * 100) / 100+' Baht');
-						$$('#check-out-grand-total').text(Math.round(total*1.17 * 100) / 100+' Baht');
-					  });
-					}
-				}
-			}
-		});
-	$$('.check-out-button').on('click' , function(){
-		mainView.router.loadPage('static/f7/html/table/wait.html');
-	});
 });
 
 function contains(a, obj) {
