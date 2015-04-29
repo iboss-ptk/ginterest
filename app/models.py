22b8670eb11bacb54e36ec0ca2eeaf073bf8d1c5
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2015-04-30 00:18:01 +0700

    activate_table debugged

--- app/models.py
+++ app/models.py
@@ -143,7 +143,7 @@ class Reservation(models.Model):
     def activate_table(table_id, customergroup_id):
         m_orderlist = Orderlist.create_new_orderlist(table_id, customergroup_id)
         m_table = DTable.objects.get(pk=table_id)
-        if m_table.status == 'u' | m_table.status == 'r':
+        if m_table.status == 'u' or m_table.status == 'r':
             return False
 
         new_sit = Sit.objects.create(
